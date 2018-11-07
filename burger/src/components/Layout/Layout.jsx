20b288e23408c1f51552e01f5ea57de0bdad311a
AReshetnyak <areshetnyak@qualium-systems.com.ua>
2018-11-07 18:38:07 +0200

    added toolbar component

--- burger/src/components/Layout/Layout.jsx
+++ burger/src/components/Layout/Layout.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Aux from "../../hoc/Aux";
 import classes from "./Layout.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 const layout = ({ children }) => {
   return (
     <Aux>
-      <div>Toolbar, SideDrawer, Backdrop</div>
+      <Toolbar/>
       <main className={classes.Content}>{children}</main>
     </Aux>
   );
